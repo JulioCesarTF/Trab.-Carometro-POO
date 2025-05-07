be890b904dd3c6859f1714f987cd59aa62fc4eb0
Júlio César Tonelini de Faria <127901667+JulioCesarTF@users.noreply.github.com>
2025-05-07 12:16:16 -0300

    Add files via upload

---
+++ controller/ControlMain.java
@@ -0,0 +1,27 @@
+package controller;
+
+/**
+ * fazer os imports das classes
+ */
+public class ControlMain {
+    
+    //criação das classes
+    
+    
+    
+
+    public ControlMain() {
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args){
+        new ControlMain();
+    }
+}
